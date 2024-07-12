2076e4866f6fb86b12de856b8f968d0b9b7ae003
Emily Twyman <twymanem@gmail.com>
2024-07-12 08:46:18 -0600

    but why

--- painAllofit/app.js
+++ painAllofit/app.js
@@ -1,19 +1,11 @@
-const port = process.env.port || 5432
-const db = require('./config/database')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
 
-// //http://localhost:3000/getUSers
-App.get('./getActors', db.getActors)
-
-App.get('/getFilmById/:id', db.getFilmById)
-
-App.use(express.json())
-App.use(express.urlencoded({extended:true}))
-
-App.listen(port, ()=>{
-    console.log('server is URLSearchParams, port:${port}');
+app.get('/', (req, res)=>{
+    res.send('Good day')
 })
 
-let dbConfig = {
-    connectionString: process.env.DB_URL,
-    ssl: {rejectUnauthorized: false}
-}
+app.listen(3000, ()=>{
+    console.log('server is up');
+})
