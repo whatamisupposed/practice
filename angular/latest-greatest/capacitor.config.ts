aadd5dc46b019429f43a07a3198bf72431860e0f
Emily Twyman <twymanem@gmail.com>
2024-10-02 09:15:38 -0600

    i like this

---
+++ angular/latest-greatest/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'latest.greatest',
+  appName: 'latest-greatest',
+  webDir: 'dist'
+};
+
+export default config;
